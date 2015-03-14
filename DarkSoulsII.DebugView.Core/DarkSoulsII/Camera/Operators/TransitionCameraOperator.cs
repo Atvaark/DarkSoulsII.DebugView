aa99cd3a1ecc88c7e2aded1c41a2262139f6aa35
Atvaark <Atvaark@users.noreply.github.com>
2015-03-14 21:07:25 +0100

    Fixed dereferencing a null-pointer in the TransitionCameraOperator

--- DarkSoulsII.DebugView.Core/DarkSoulsII/Camera/Operators/TransitionCameraOperator.cs
+++ DarkSoulsII.DebugView.Core/DarkSoulsII/Camera/Operators/TransitionCameraOperator.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using DarkSoulsII.DebugView.Core.DarkSoulsII.Rtti;
+using DarkSoulsII.DebugView.Core.Exceptions;
 
 namespace DarkSoulsII.DebugView.Core.DarkSoulsII.Camera.Operators
 {
@@ -18,16 +20,18 @@ namespace DarkSoulsII.DebugView.Core.DarkSoulsII.Camera.Operators
         public new TransitionCameraOperator Read(IPointerFactory pointerFactory, IReader reader, int address, bool relative = false)
         {
             base.Read(pointerFactory, reader, address, relative);
+            // TODO: Check if there is an initialized flag in this class
+            var cameraOperatorsPointer = GenericPointer.Create(reader, address + 0x00B4, relative);
+            if (cameraOperatorsPointer.IsNull)
+                return this;
 
-            // BUG: Invalid pointer during the game initialization states
-            var cameraOperators = GenericPointer.Create(reader, address + 0x00B4, relative)
+            var cameraOperators = cameraOperatorsPointer
                 .Unbox(reader, (r, a) => r.ReadInt32(10, a))
                 .Select(a => CameraOperatorHelper.Instance.ResolvePointer(pointerFactory, reader, a).Unbox(pointerFactory, reader));
             CameraOperators.AddRange(cameraOperators);
-
             var index = reader.ReadInt32(address + 0x00C4, relative);
             CurrentOperator = CameraOperators.ElementAt(index);
-            CurrentOperatorType = (CameraOperatorType) index;
+            CurrentOperatorType = (CameraOperatorType)index;
             int freeze = reader.ReadInt32(address + 0x00C8, relative);
             int index2 = reader.ReadInt32(address + 0x00CC, relative);
             return this;
